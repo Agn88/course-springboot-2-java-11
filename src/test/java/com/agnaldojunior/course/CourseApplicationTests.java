38a10b577021d9ff931e11991ea4df6e3f8bb98c
unknown <agnaldo.junior@unoesc.edu.br>
2020-08-23 22:18:06 -0300

    Project Created

---
+++ src/test/java/com/agnaldojunior/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnaldojunior.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
